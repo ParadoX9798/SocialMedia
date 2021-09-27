afe621739340804aec31488237bf6411cf8a5d1d
ParadoX <youneskazemi9798@gmail.com>
2021-09-27 14:33:10 +0330

    delete post

--- post/views.py
+++ post/views.py
@@ -4,6 +4,7 @@ from .forms import PostForm
 from django.contrib import messages
 from django.utils.text import slugify
 from django.contrib.auth.decorators import login_required
+from django.contrib.auth.models import User
 
 
 def all_posts(request):
@@ -33,4 +34,16 @@ def add_post(request, id):
             form = PostForm()
         return render(request, 'post/add_post.html', {"form": form})
     else:
+        return redirect("post:all_posts")
+
+
+@login_required
+def delete_post(request, user_id, post_id):
+    if user_id == request.user.id:
+        user = get_object_or_404(User, id=user_id)
+        post = get_object_or_404(Post, user=user, id=post_id)
+        post.delete()
+        messages.success(request, "Your Post Deleted Successfully!", "success")
+        return redirect("accounts:user_dashboard", user_id)
+    else :
         return redirect("post:all_posts")

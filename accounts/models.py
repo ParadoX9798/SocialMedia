5df2875c0c9bdc0f67f479b4ef63415fd7ff3446
ParadoX <youneskazemi9798@gmail.com>
2021-10-02 10:09:05 +0330

    Ajax Follow

--- accounts/models.py
+++ accounts/models.py
@@ -7,6 +7,7 @@ class Profile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
     age = models.PositiveSmallIntegerField(null=True, blank=True)
     bio = models.TextField(null=True, blank=True)
+    phone = models.PositiveIntegerField(null=True, blank=True)
 
     def __str__(self):
         return f'{self.user.username}'
@@ -19,3 +20,15 @@ def save_profile(sender, **kwargs):
 
 
 post_save.connect(receiver=save_profile, sender=User)
+
+
+class Relations(models.Model):
+    from_user = models.ForeignKey(User, on_delete=models.CASCADE, related_name="follower")
+    to_user = models.ForeignKey(User, on_delete=models.CASCADE, related_name="following")
+    created = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        ordering = ("-created",)
+
+    def __str__(self):
+        return f'{self.from_user} following {self.to_user}'
